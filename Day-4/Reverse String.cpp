5365894562b5b11d098912b504082f113dabc505
Rohitdubey67 <31805232+Rohitdubey67@users.noreply.github.com>
2020-06-04 13:30:01 +0530

    Create Reverse String.cpp

---
+++ Day-4/Reverse String.cpp
@@ -0,0 +1,36 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
+
+Example 1:
+
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+
+/*
+Time: O(N)
+Space: O(1)
+Comcept: two pointer method
+*/
+
+void reverseString(vector<char>& s) {
+        int n=s.size();
+        int l=0,r=n-1;
+        while(l<r)
+        {
+            swap(s[l],s[r]);
+            l++;
+            r--;
+        }
+    }
+    
